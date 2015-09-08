0124893c3320e876aaa98e0131b53dccccdea05f
Hyeon-Seob Seo <hsseo417@gmail.com>
2015-09-09 00:03:19 +0900

    Update UI

--- app/src/main/java/com/example/hyeonseob/beacontriangulation/Intro/MainActivity.java
+++ app/src/main/java/com/example/hyeonseob/beacontriangulation/Intro/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.hyeonseob.beacontriangulation.Intro;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,49 +21,13 @@ public class MainActivity extends Activity {
     public static final boolean ENABLE_BACKGROUND_RANGING_TIMEOUT = true;
     public static final boolean DISCONTINUOUS_SCAN = false;
 
-    private static final int REQUEST_ENABLE_BT = 1;
-
-    private BluetoothManager mBluetoothManager;
-    private BluetoothAdapter mBluetoothAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //If a user device turns off bluetooth, request to turn it on.
-        mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = mBluetoothManager.getAdapter();
-
-        if(mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
-        }
-
         DeviceManager mDeviceManager = new DeviceManager(getApplicationContext());
-        ((TextView)findViewById(R.id.androidIdTextView)).setText(mDeviceManager.getDeviceString());
-    }
-
-    @Override
-     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
-                //If the request to turn on bluetooth is denied, the app will be finished.
-                finish();
-                return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onResume() {
-        Log.i("MainActivity", "onResume()");
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.i("MainActivity", "onDestroy");
-        super.onDestroy();
+        ((TextView)findViewById(R.id.androidIdTextView)).setText("Device: "+mDeviceManager.getDeviceString());
     }
 
     public void onButtonClicked(View v) {
